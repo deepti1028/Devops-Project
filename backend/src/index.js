719c778d008646380da4f628b4b7d10ee5849972
Harsh Sharma <harshsharma20503@gmail.com>
2024-10-07 14:07:46 +0530

    containerised the application with docker

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import cors from "cors";
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -7,10 +11,36 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(express.static());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-app.get("/", (req, res) => {
-  res.send("Hello ");
+const origins = process.env.CORS_ORIGIN?.split(",") || [];
+
+// Set up CORS middleware
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Check if the request origin is allowed
+      if (!origin || origins.includes(origin)) {
+        callback(null, true); // Allow the request
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allow sending cookies across domains
+  })
+);
+
+app.get("/api", (req, res) => {
+  console.log("Inside the Home route");
+  return res.json({ message: "Hello From Api Home route" });
+});
+
+app.use(express.static(path.join(__dirname, "public", "dist")));
+// Catch-all route should come after all other routes
+app.get("/*", (req, res) => {
+  console.log("Inside the catch-all route");
+  res.sendFile(path.join(__dirname, "public", "dist", "index.html"));
 });
 
 app.listen(PORT, () => {

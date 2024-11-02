1e292d8b6779d53f62157b4c0d2ee0c6459259ee
Aranya Maji <aranyamaji28@gmail.com>
2024-11-02 10:38:05 +0530

    Updated app.js and index.js

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import chatRouter from "./routes/chat.routes.js";
+import messageRouter from "./routes/message.routes.js";
+import { notFound, errorHandler } from "./middlewares/error.middleware.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); // to parse json in body
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // to parse url
+app.use(express.static("public")); // to use static public folder
+app.use(cookieParser()); // to enable CRUD operation on browser cookies
+
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/chat", chatRouter);
+app.use("/api/message", messageRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+export { app };

3539a2d9b077de35d96ce746687c36716a7e0618
Deepti Jain <deeptihnd000@gmail.com>
2024-11-05 13:23:51 +0530

    added context and installed necessary dependencies

---
+++ frontend/src/Context/ChatProvider.jsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const ChatContext = createContext();
+
+const ChatProvider = ({ children }) => {
+  const [selectedChat, setSelectedChat] = useState(null);
+  const [user, setUser] = useState(null);
+  const [notification, setNotification] = useState([]);
+  const [chats, setChats] = useState([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleUrlChange = () => {
+      // Your logic to run when there is a change in the URL
+      console.log("URL has changed:", window.location.href);
+    };
+    window.addEventListener("popstate", handleUrlChange);
+    const userInfoString = localStorage.getItem("userInfo");
+    if (userInfoString) {
+      try {
+        const userInfo = JSON.parse(userInfoString);
+        setUser(userInfo);
+      } catch (error) {
+        console.error("Error parsing userInfo:", error);
+      }
+    } else {
+      navigate("/");
+    }
+    return () => {
+      window.removeEventListener("popstate", handleUrlChange);
+    };
+  }, [window.location.href]);
+
+  return (
+    <ChatContext.Provider
+      value={{
+        selectedChat,
+        setSelectedChat,
+        user,
+        setUser,
+        notification,
+        setNotification,
+        chats,
+        setChats,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const ChatState = () => {
+  return useContext(ChatContext);
+};
+
+export default ChatProvider;
